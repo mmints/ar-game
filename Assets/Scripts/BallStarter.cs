de31094c37a026e03822f951afaaace9dbce1fad
Mark Oliver Mints <mmints@uni-koblenz.de>
2017-06-21 01:21:52 +0200

    Add buggy strarter animation

--- Assets/Scripts/BallStarter.cs
+++ Assets/Scripts/BallStarter.cs
@@ -4,29 +4,26 @@ using UnityEngine;
 
 public class BallStarter : MonoBehaviour {
 
-	private float force;
 	public float maxForce;
 
+	private float force;
 	private Rigidbody rb_Ball;
+	private Vector3 restPosition;
 
 	private int debugCounter;
 
-	// Use this for initialization
-//	void Start () {
-//		debugCounter = 0;
-//		force = 0;
-//	}
+	void Start()
+	{
+		foreach (Transform child in transform) {
+			restPosition = child.position;
+		}
 
-	// Update is called once per frame
-	void Update () {
-		
 	}
 
 	void OnTriggerEnter (Collider ball)
 	{
 		debugCounter = 0;
 		force = 0.0f;
-		Debug.Log (ball.gameObject.name + " ball enter start position!");	
 	}
 
 	void OnTriggerStay (Collider ball)
@@ -36,14 +33,27 @@ public class BallStarter : MonoBehaviour {
 		rb_Ball = ball.gameObject.GetComponent<Rigidbody>();
 
 		if (Input.GetAxis ("Fire") == 1 && force <= maxForce) {
-			force += 0.1f;
+			force += 0.35f;
+
 			Debug.Log ("Force: "  + force);
 		}
-		else{
+		else {
 			rb_Ball.AddForce (0, 0, force, ForceMode.Impulse);
 			Debug.Log ("Fire ball!");
 		}
+	}
 
+	void Update(){
+		if (Input.GetAxis ("Fire") == 1) {
+			foreach (Transform child in transform) {
+				child.position -= Vector3.forward * 0.1F;
+			}
+		}
+		else {
+			foreach (Transform child in transform) {
+				child.position = restPosition;
+			}
+		}
 
 	}
 
