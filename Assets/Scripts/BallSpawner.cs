596de7cd8e63bb32bbee25a797d0aa2c62368a32
Mark Oliver Mints <mmints@uni-koblenz.de>
2017-06-20 17:54:55 +0200

    Add modular spawn point

--- Assets/Scripts/BallSpawner.cs
+++ Assets/Scripts/BallSpawner.cs
@@ -4,11 +4,14 @@ using System.Collections;
 public class BallSpawner : MonoBehaviour {
 
 	public GameObject Ball;
-	public Vector3 position;
+	// public Vector3 position;
+
+	public GameObject Spawn;
 
 	void Start () {
-			Instantiate (Ball, position, Quaternion.identity);
-			Debug.Log ("Create new ball.");
+		// Instantiate (Ball, position, Quaternion.identity);
+		Instantiate (Ball, Spawn.transform.position, Quaternion.identity);
+		Debug.Log ("Create new ball.");
 	}
 
 }
