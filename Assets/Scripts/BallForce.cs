abba4720ae4d917b7cbc5ec6d4ed1f0995aefdc4
Mark Oliver Mints <mmints@uni-koblenz.de>
2017-06-20 20:01:08 +0200

    Implements start functionality

--- Assets/Scripts/BallForce.cs
+++ Assets/Scripts/BallForce.cs
@@ -6,10 +6,19 @@ public class BallForce : MonoBehaviour {
 	public float force;
 
 	private Rigidbody rb_Ball;
-
+	private int time;
 	// Use this for initialization
 	void Start () {
 		rb_Ball = GetComponent<Rigidbody>();
-		//rb_Ball.AddForce (0, 0, force, ForceMode.Impulse);
-	}	
+		time = 0;
+	}
+
+	void Update () {
+		if (Input.GetAxis ("Fire") == 1 && time <= 10) {
+			rb_Ball.AddForce (0, 0, force, ForceMode.Impulse);
+			time += 1;
+			Debug.Log ("Add force " + time + " times!");
+		}
+
+	}
 }
---
+++ Assets/Scripts/BallStarter.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallStarter : MonoBehaviour {
+
+	private float force;
+	public float maxForce;
+
+	private Rigidbody rb_Ball;
+
+	private int debugCounter;
+
+	// Use this for initialization
+//	void Start () {
+//		debugCounter = 0;
+//		force = 0;
+//	}
+
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void OnTriggerEnter (Collider ball)
+	{
+		debugCounter = 0;
+		force = 0.0f;
+		Debug.Log (ball.gameObject.name + " ball enter start position!");	
+	}
+
+	void OnTriggerStay (Collider ball)
+	{
+		debugCounter += 1;
+		Debug.Log (ball.gameObject.name + " stay on start position " + "(" + debugCounter + ")");
+		rb_Ball = ball.gameObject.GetComponent<Rigidbody>();
+
+		if (Input.GetAxis ("Fire") == 1 && force <= maxForce) {
+			force += 0.1f;
+			Debug.Log ("Force: "  + force);
+		}
+		else{
+			rb_Ball.AddForce (0, 0, force, ForceMode.Impulse);
+			Debug.Log ("Fire ball!");
+		}
+
+
+	}
+
+}
