98ede5c4f9b0f6916a486cbf5e0efe3f944bcaaf
Pablo Delgado Krämer <private@pablode.com>
2017-07-25 11:29:35 +0200

    Add sync script

---
+++ Assets/Scripts/TransformSync.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TransformSync : MonoBehaviour {
+
+    public GameObject obtainTransFrom;
+
+	void FixedUpdate() {
+		var remoteTrans = obtainTransFrom.transform;
+		transform.position = remoteTrans.position;
+		transform.rotation = remoteTrans.rotation;
+	}
+}
