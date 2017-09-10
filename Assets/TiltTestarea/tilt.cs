77838dc20429c4dcaa8707c47f0479e9192fd56f
Mark Oliver Mints <mmints@uni-koblenz.de>
2017-09-10 18:30:45 +0200

    Add testing scene and script for tilt development

--- Assets/TiltTestarea/tilt.cs
+++ Assets/TiltTestarea/tilt.cs
@@ -2,22 +2,22 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class TiltController : MonoBehaviour {
+public class tilt : MonoBehaviour {
 
-    public string inputLeft;
-    public string inputRight;
-    public float force;
+	public string inputLeft;
+	public string inputRight;
+	public float force;
 
-    private Rigidbody rigidBody;
-    private int tiltCounter;
+	private Rigidbody rigidBody;
+	private int tiltCounter;
 
-    void Start() {
-        tiltCounter = 0;
-        rigidBody = GetComponent<Rigidbody>();
-    }
+	void Start() {
+		tiltCounter = 0;
+		rigidBody = GetComponent<Rigidbody>();
+	}
 
-    void Update() {
-        /*if (Input.GetAxis(inputLeft) == 1 && tiltCounter < 3) {
+	void Update() {
+		if (Input.GetAxis(inputLeft) == 1 && tiltCounter < 3) {
             tiltCounter++;
             rigidBody.AddForce(-force, 0, 0, ForceMode.Impulse);
             Debug.Log("Tilt left (" + tiltCounter + ")");
@@ -26,6 +26,6 @@ public class TiltController : MonoBehaviour {
             tiltCounter++;
             rigidBody.AddForce(force, 0, 0, ForceMode.Impulse);
             Debug.Log("Tilt right (" + tiltCounter + ")");
-        }*/
-    }
+        }
+	}
 }
