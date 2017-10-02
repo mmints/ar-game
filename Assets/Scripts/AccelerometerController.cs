c2eff87611d705042ce4bede6a7011fce0bd0c62
Mark Oliver Mints <mmints@uni-koblenz.de>
2017-10-02 18:36:15 +0200

    Implements tilt

---
+++ Assets/Scripts/AccelerometerController.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//use this script with the ball prefab
+
+public class AccelerometerController : MonoBehaviour {
+
+	public float force = 100;
+	private float acc;
+	private Rigidbody ball;
+	private float vz;
+
+	void Start() {
+		ball = GetComponent<Rigidbody>();
+	}
+
+	void Update ()
+	{
+		acc = Input.acceleration.x;
+		vz = acc / -acc;
+
+		if (acc > 0.2 || acc < -0.2) {
+			ball.AddForce (vz * force, 0, 0, ForceMode.Impulse);
+		}
+
+
+	}
+}
